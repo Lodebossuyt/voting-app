ea14f3274a6dca170b6ae57a506b251672f82ee3
Lodebossuyt <lodebossuyt@hotmail.com>
2022-06-30 21:29:19 +0200

    tailwindcss design

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                'gray-background': '#f7f8fc',
+            },
+            fontFamily: {
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+            maxWidth: {
+                custom: '62.5rem',
+            },
+            spacing: {
+                70: '17.5rem',
+                175: '43.75rem',
+            }
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
