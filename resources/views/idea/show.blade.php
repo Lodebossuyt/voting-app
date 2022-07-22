2d7608e29e2e763b3270fbee6b8263e9519fc598
Lodebossuyt <lodebossuyt@hotmail.com>
2022-07-22 12:38:32 +0200

    back button goes to previous url or url full

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
    <div>
-       <a href="/" class="flex items-center font-semibold hover:underline">
+       <a href="{{ $backUrl }}" class="flex items-center font-semibold hover:underline">
            <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
            </svg>
            <span class="ml-2">
-               All ideas
+               All ideas (or back to chosen categories + filters)
            </span>
        </a>
    </div>
--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -9,14 +9,14 @@
             </select>
         </div>
         <div class="w-full md:w-1/3">
-            <select name="other_filters" id="category" class="w-full rounded-xl px-4 py-2 border-none">
-                <option value="Category one">Filter One</option>
-                <option value="Category one">Filter Two</option>
-                <option value="Category one">Filter Three</option>
+            <select wire:model="filter" name="other_filters" id="category" class="w-full rounded-xl px-4 py-2 border-none">
+                <option value="No Filter">No Filter</option>
+                <option value="Top Voted">Top Voted</option>
+                <option value="My Ideas">My Ideas</option>
             </select>
         </div>
         <div class="w-full md:w-2/3 relative">
-            <input type="search" placeholder="find an idea"
+            <input wire:model="search" type="search" placeholder="find an idea"
                    class="w-full rounded-xl border-none bg-white px-4 py-2 pl-8 placeholder:text-gray-900">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -30,9 +30,16 @@
 
     {{--Start Ideascontainer--}}
     <div class="ideas-container space-y-6 my-6">
-        @foreach($ideas as $idea)
+        @forelse($ideas as $idea)
             @livewire('idea-index', key($idea->id) , ['idea'=>$idea, 'votesCount'=>$idea->votes_count])
-        @endforeach
+        @empty
+            <div class="mx-auto w-70 mt-12 flex ">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                </svg>
+                <div class="text-gray-400 text-center font-bold mt-1">No ideas were found...</div>
+            </div>
+        @endforelse
     </div>
     {{--End Ideascontainer--}}
 
